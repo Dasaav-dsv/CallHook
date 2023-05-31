b6fa7bb7c14cdf8f6ac646c6d5ed8d0b7e06e764
Dasaav-dsv <simplydasaav@gmail.com>
2023-05-31 23:28:32 +0200

    Support for more than just E8/E9 calls and jumps

--- include/CallPatcherVEH.h
+++ include/CallPatcherVEH.h
@@ -14,7 +14,8 @@ namespace CallHook {
 		struct PatchJob {
 			void* callAddress;
 			int tgtDisp;
-			bool isJmp;
+			uint8_t firstByte;
+			int immOffset;
 		};
 
 	public:
@@ -24,14 +25,14 @@ namespace CallHook {
 		}
 
 		// Patch a call or jmp instruction at a specified address in memory.
-		bool patchCall(void* callAddress, void* tgtAddress, bool isJmp = false) {
+		bool patchCall(void* callAddress, void* tgtAddress, uint8_t firstByte, int immOffset) {
 			int64_t disp = reinterpret_cast<uintptr_t>(tgtAddress) - reinterpret_cast<uintptr_t>(callAddress) - 5;
 			// To encode the new call target, its displacement must fit in a signed 32 bit integer.
 			if (std::abs(disp) > INT_MAX) {
 				Logger::get().log("Error when patching call at %p, target address %p is out of range", callAddress, tgtAddress);
 				return false;
 			}
-			CallPatcher::jobs.emplace_back(PatchJob{ callAddress, static_cast<int>(disp), isJmp });
+			CallPatcher::jobs.emplace_back(PatchJob{ callAddress, static_cast<int>(disp), firstByte, immOffset });
 			// Prepare the VEH and place a software breakpoint on the first byte of the call.
 			// This is important because the 32 bit integer displacement encoded in the call may not be 
 			// aligned on a 4 byte boundary. This means writes to that memory may not be atomic.
@@ -44,8 +45,8 @@ namespace CallHook {
 			// No matter if the breakpoint was hit and the address has already been patched,
 			// patch it again. If it has not been patched by the VEH before, it will be patched now,
 			// if it was already patched by the VEH, nothing will happen.
-			CallPatcher::write(reinterpret_cast<uint8_t*>(callAddress) + 1, static_cast<int>(disp));
-			CallPatcher::write(callAddress, static_cast<uint8_t>(0xE8 | isJmp));
+			CallPatcher::write(reinterpret_cast<uint8_t*>(callAddress) + immOffset, static_cast<int>(disp));
+			CallPatcher::write(callAddress, firstByte);
 			FlushInstructionCache(GetCurrentProcess(), NULL, 0);
 			// Remove the job.
 			for (auto iter = CallPatcher::jobs.begin(); iter != CallPatcher::jobs.end(); ++iter) {
@@ -84,8 +85,8 @@ namespace CallHook {
 			for (auto& job : CallPatcher::jobs) {
 				if (exAddress != reinterpret_cast<uint8_t*>(job.callAddress)) continue;
 				// Write the displacement and restore the call/jmp byte.
-				CallPatcher::write(exAddress + 1, job.tgtDisp);
-				CallPatcher::write(exAddress, static_cast<uint8_t>(0xE8 | job.isJmp));
+				CallPatcher::write(exAddress + job.immOffset, job.tgtDisp);
+				CallPatcher::write(exAddress, job.firstByte);
 				FlushInstructionCache(GetCurrentProcess(), NULL, 0);
 				return EXCEPTION_CONTINUE_EXECUTION;
 			}
